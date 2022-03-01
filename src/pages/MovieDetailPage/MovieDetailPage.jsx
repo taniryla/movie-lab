1bfa2ecfbad754a524def6022991bb55d4937830
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-01 10:53:38 -0800

    Part 2 Updated

--- src/pages/MovieDetailPage/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage/MovieDetailPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./MovieDetailPage.css";
 import { useParams } from "react-router-dom";
 
 export default function MovieDetailPage({ movies }) {
@@ -11,6 +11,11 @@ export default function MovieDetailPage({ movies }) {
       <h1>{movie.title}</h1>
       <h1>Date: {movie.dateReleased}</h1>
       <h3>Cast Members: {movie.cast.join(", ")}</h3>
+      <div className="poster">
+        <div style={{ backgroundImage: `url(${movie.posterPath})` }}></div>
+        <div style={{ backgroundImage: `url(${movie.posterPath})` }}></div>
+        <div style={{ backgroundImage: `url(${movie.posterPath})` }}></div>
+      </div>
     </div>
   );
 }
